90dbe05b6d64977a9dafdbabf21e340bd95e75c7
good-atom <164239583+good-atom@users.noreply.github.com>
2024-05-11 23:07:00 +0300

    Add files via upload

---
+++ send.php
@@ -0,0 +1,23 @@
+<?php
+
+$name = $_POST['name'];
+$telegram = $_POST['telegram'];
+$token = "7127258591:AAH6NVA0V_Qy7KyIzNNPSbuvrKiZN_RcUxY";
+$chat_id = "-4259134876";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телеграмм: ' => $telegram,
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank_you.html');
+} else {
+  echo "Error";
+}
+?>
