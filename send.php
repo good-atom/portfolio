fff87429a4b44b8d04fe62510c5451dbbbecfc4c
good-atom <164239583+good-atom@users.noreply.github.com>
2024-05-12 13:52:07 +0300

    Update send.php

--- send.php
+++ send.php
@@ -4,8 +4,8 @@
 
 $name = $_POST['name'];
 $telegram = $_POST['telegram'];
-$token = "7192004641:AAF6XF00Z4Z_X7kSQCs5SbyetSIfSKVdxIc";
-$chat_id = "-4282747028";
+$token = "7028989155:AAHMubg8mSDT-6H0SMNVXQnoeW2zrqW9GiQ";
+$chat_id = "-1002058942409";
 $arr = array(
   'Имя пользователя: ' => $name,
   'Телеграмм: ' => $telegram,
@@ -18,12 +18,8 @@ foreach($arr as $key => $value) {
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 if ($sendToTelegram) {
-        alert('Спасибо! Ваша заявка принята. Мы свяжемся с вами в ближайшее время.');
-        /*header('Location: my_web.html'); перебрасывает на my_web*/
-    }
-
-//А здесь сообщение об ошибке при отправке
-    else {
-        alert('Что-то пошло не так. Попробуйте отправить форму ещё раз.');
-    }
+  header('Location: success.html');
+} else {
+  echo "Error";
+}
 ?>
