fe59579d6614ca013f02e1ec197e4fe500a4c0b1
good-atom <164239583+good-atom@users.noreply.github.com>
2024-05-13 13:56:11 +0300

    Update txt.php

--- txt.php
+++ txt.php
@@ -4,8 +4,8 @@ $filename = 'form_responses.txt';
 if (!empty($_POST)) {
     $record = [
         // перечисляются поля формы
-        'first_name' => filter_input(INPUT_POST, 'first_name', FILTER_SANITIZE_STRING),
-        'day_of_week' => filter_input(INPUT_POST, 'day_of_week', FILTER_SANITIZE_NUMBER_INT),
+        'name' => filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING),
+        'telegram' => filter_input(INPUT_POST, 'telegram', FILTER_SANITIZE_NUMBER_INT),
     ];
 
     $recordString = implode('; ', $record) . PHP_EOL; // склеить значения через точку с запятой
